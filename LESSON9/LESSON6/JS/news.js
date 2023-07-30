ddcf3b8a02addc63c1de1fd1c6987e778f8bcdb7
manhthang0411 <137802090+manhthang0411@users.noreply.github.com>
2023-07-31 03:38:11 +0700

    Add files via upload

---
+++ LESSON9/LESSON6/JS/news.js
@@ -0,0 +1,153 @@
+
+
+var listNews = [
+    {
+        id : 1,
+        title : "tin tức số 1",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 2,
+        title : "tin tức số 2",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "../Images/news.jpg",
+    },
+    
+    {
+        id : 3,
+        title : "tin tức số 3",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 4,
+        title : "demo",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 5,
+        title : "demo 2",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 6,
+        title : "tin 6",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 7,
+        title : "tin 7",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    }
+]
+
+var listNews2 = [
+    {
+        id : 8,
+        title : "tin tức số 8",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 2,
+        title : "tin tức số 0",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "../Images/news.jpg",
+    },
+    
+    {
+        id : 3,
+        title : "tin tức số 3",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 4,
+        title : "demo",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 5,
+        title : "demo 66",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 6,
+        title : "tin 6",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    },
+    
+    {
+        id : 7,
+        title : "tin 7",
+        content: "Trà thảo dược được coi là một loại thực phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe phẩm chức năng có tác dụng tốt cho sức khỏe. Tuy nhiê",
+        image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
+    }
+]
+function setView(tsst) {
+    var view = `<div class="news-item">
+                  <div class="news-img">
+                      <img class="w-100" src="${tsst.image}" alt="item">
+                  </div>
+                  <div class="news-conten">
+                      <h3 class="news-title">${tsst.title}</h3>
+                      <p class="news-content">${tsst.content}</p>
+                  </div>
+                </div>`;
+    return view;
+  }
+  
+  function setListNews(array, search) {
+    var count = array.length;
+    $('.news-page-content .list-news').html('');
+    for (let i = 0; i < count; i++) {
+      if (array[i].title.toLowerCase().includes(search.toLowerCase()) || search === "") {
+        var view = setView(array[i]);
+        $('.news-page-content .list-news').append(view);
+      }
+    }
+  }
+  
+  function searchItem() {
+    var title = $('.form-search #search').val();
+    console.log("search: ", title);
+    setListNews(listNews, title);
+  }
+  
+  setListNews(listNews, "");
+  
+var pageIndex  = 1;
+var pageSize = 3;
+// 1: 0 1 2; 0          => (pageIndex - 1)* pageSize  || pageIndex * pageSize
+// 2: 3 4 5;
+// 3: 6 
+
+function setPageNews(array, pageNumber){
+    var count = array.length ;
+    var start = (pageNumber - 1)* pageSize;
+    var end =  pageNumber * pageSize;
+    $('.news-page-content .list-news').html('');
+    for(let i = start; (i < end) && (i< count+1) ; i++){
+        setView(array[i])
+    }
+}
+function changePageNumber(pageNumber){
+    setPageNews(listNews, pageNumber)
+}
