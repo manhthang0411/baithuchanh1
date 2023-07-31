a29705cba169de4e8e4d4a08913e1ef8bdb0ee1a
manhthang0411 <137802090+manhthang0411@users.noreply.github.com>
2023-07-31 19:09:08 +0700

    Update news.js

--- LESSON9/LESSON6/JS/news.js
+++ LESSON9/LESSON6/JS/news.js
@@ -101,53 +101,107 @@ var listNews2 = [
         image : "http://acatea.vn/Content/uploads/news/16_%20Am%20tra%203%20coc.png",
     }
 ]
-function setView(tsst) {
+function setView(tsst){
     var view = `<div class="news-item">
-                  <div class="news-img">
-                      <img class="w-100" src="${tsst.image}" alt="item">
-                  </div>
-                  <div class="news-conten">
-                      <h3 class="news-title">${tsst.title}</h3>
-                      <p class="news-content">${tsst.content}</p>
-                  </div>
+                    <div class="news-img">
+                        <img class="w-100" src="${tsst.image}" alt="item">
+                    </div>
+                    <div class="news-conten">
+                        <h3 class="news-title">${tsst.title}</h3>
+                        <p class="news-content">${tsst.content}</p>
+                    </div>
+
                 </div>`;
-    return view;
-  }
-  
-  function setListNews(array, search) {
-    var count = array.length;
+
+    $('.news-page-content .list-news').append(view);
+}
+
+function setListNews(array, search){
+    var count = array.length ;
     $('.news-page-content .list-news').html('');
-    for (let i = 0; i < count; i++) {
-      if (array[i].title.toLowerCase().includes(search.toLowerCase()) || search === "") {
-        var view = setView(array[i]);
-        $('.news-page-content .list-news').append(view);
-      }
+    for(let i = 0 ; i < count ; i++){
+        // let test = listNews[i].image
+        if(array[i].title.toLowerCase().includes(search.toLowerCase()) || search == ""){
+            setView(array[i])
+        }
     }
-  }
-  
-  function searchItem() {
-    var title = $('.form-search #search').val();
+}
+function searchItem(){
+    debugger;
+    // var title = document.getElementById('search').value;
+    var title = $('.form-search #search').val(); //  $('.form-search #search-1').val(); 
     console.log("search: ", title);
     setListNews(listNews, title);
-  }
-  
-  setListNews(listNews, "");
-  
+    // ==> syntax: $ + ('<selector>') + . + action(param)
+}
+setListNews(listNews, "");
+
 var pageIndex  = 1;
 var pageSize = 3;
 // 1: 0 1 2; 0          => (pageIndex - 1)* pageSize  || pageIndex * pageSize
 // 2: 3 4 5;
 // 3: 6 
 
-function setPageNews(array, pageNumber){
-    var count = array.length ;
-    var start = (pageNumber - 1)* pageSize;
-    var end =  pageNumber * pageSize;
-    $('.news-page-content .list-news').html('');
-    for(let i = start; (i < end) && (i< count+1) ; i++){
-        setView(array[i])
+var displayedNewsIds = [];
+
+
+function setPageNews(array, pageNumber) {
+  var start = (pageNumber - 1) * pageSize;
+  var end = start + pageSize;
+  var newsContainer = $('.news-page-content .list-news');
+  newsContainer.html('');
+
+  for (let i = start; i < end && i < array.length; i++) {
+    if (!displayedNewsIds.includes(array[i].id)) {
+      setView(array[i]);
+      displayedNewsIds.push(array[i].id);
     }
+  }
 }
-function changePageNumber(pageNumber){
-    setPageNews(listNews, pageNumber)
+
+
+function renderPaginationButtons() {
+  var paginationContainer = $('.pagination-container');
+  paginationContainer.html('');
+
+  for (let i = 1; i <= totalPages; i++) {
+    paginationContainer.append(`<button class="page-btn page-number" data-page="${i}">${i}</button>`);
+  }
+
+  $('.page-number').click(function () {
+    var pageNumber = parseInt($(this).attr('data-page'));
+    setPageNews(listNews, pageNumber);
+    setActivePageButton(pageNumber);
+  });
 }
+function setActivePageButton(pageNumber) {
+  $('.page-number').removeClass('active');
+  $(`.page-number[data-page="${pageNumber}"]`).addClass('active');
+}
+
+
+function goToPreviousPage() {
+  if (pageIndex > 1) {
+    pageIndex--;
+    setPageNews(listNews, pageIndex);
+    setActivePageButton(pageIndex);
+  }
+}
+
+
+function goToNextPage() {
+  if (pageIndex < totalPages) {
+    pageIndex++;
+    setPageNews(listNews, pageIndex);
+    setActivePageButton(pageIndex);
+  }
+}
+
+$('.prev-page').click(goToPreviousPage);
+$('.next-page').click(goToNextPage);
+
+
+var totalPages = Math.ceil(listNews.length / pageSize);
+renderPaginationButtons();
+setPageNews(listNews, pageIndex);
+setActivePageButton(pageIndex);
